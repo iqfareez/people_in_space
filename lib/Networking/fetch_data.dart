08e2268dc8e6c26360c1e15a389640a20bdeb9ee
iqfareez <iqmal3@outlook.com>
2021-06-20 12:18:21 +0800

    :rocket: Deplo web 1.0.0-pre+1

--- lib/Networking/fetch_data.dart
+++ lib/Networking/fetch_data.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:people_in_space/Networking/astros_model.dart';
 import 'package:people_in_space/Networking/bing_search_model.dart';
-import 'package:people_in_space/SECRETS.dart';
+// import 'package:people_in_space/SECRETS.dart';
 
 class FetchData {
   static Future<AstrosModel> getAstros() async {
@@ -19,12 +19,14 @@ class FetchData {
 
   static Future<BingImageResponse> getAstrosImage() async {
     final response = await http.get(
-        Uri.https(
-          'api.bing.microsoft.com',
-          '/v7.0/images/search',
-          {'q': 'meow', 'count': '5'},
-        ),
-        headers: {'Ocp-Apim-Subscription-Key': key1});
+      Uri.https(
+        'api.bing.microsoft.com',
+        '/v7.0/images/search',
+        {'q': 'meow', 'count': '5'},
+      ),
+      //TODO: Add key
+      // headers: {'Ocp-Apim-Subscription-Key': key1},
+    );
 
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
