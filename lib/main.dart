a451419dc5d8bdc5bff9277619d603a3ea106789
iqfareez <iqmal3@outlook.com>
2022-08-04 10:46:31 +0800

    :lipstick: Use material 3 design

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: GoogleFonts.firaSans().fontFamily,
+        useMaterial3: true,
       ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.system,
