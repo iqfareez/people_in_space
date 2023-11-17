4dd6b9a2f6e6ddd40eb305ef46b12d378ff0a468
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-11-18 01:38:36 +0800

    :sparkles: Add ability to properly zoom

--- lib/views/image_viewer.dart
+++ lib/views/image_viewer.dart
@@ -1,21 +1,89 @@
 import 'package:flutter/material.dart';
 
-class ImageViewer extends StatelessWidget {
+class ImageViewer extends StatefulWidget {
   const ImageViewer({super.key, required this.name, required this.imageSrc});
 
   final String imageSrc;
   final String name;
 
   @override
+  State<ImageViewer> createState() => _ImageViewerState();
+}
+
+class _ImageViewerState extends State<ImageViewer>
+    with TickerProviderStateMixin {
+  final TransformationController _transformationController =
+      TransformationController();
+  Animation<Matrix4>? _animationReset;
+  AnimationController? _controllerReset;
+
+  void _onAnimateReset() {
+    _transformationController.value = _animationReset!.value;
+    if (!_controllerReset!.isAnimating) {
+      _animationReset?.removeListener(_onAnimateReset);
+      _animationReset = null;
+      _controllerReset?.reset();
+    }
+  }
+
+  void _animateResetInitialize() {
+    _controllerReset?.reset();
+    _animationReset = Matrix4Tween(
+      begin: _transformationController.value,
+      end: Matrix4.identity(),
+    ).animate(_controllerReset!);
+    _animationReset?.addListener(_onAnimateReset);
+    _controllerReset!.forward();
+  }
+
+// Stop a running reset to home transform animation.
+  void _animateResetStop() {
+    _controllerReset!.stop();
+    _animationReset?.removeListener(_onAnimateReset);
+    _animationReset = null;
+    _controllerReset!.reset();
+  }
+
+  void _onInteractionStart(ScaleStartDetails details) {
+    // If the user tries to cause a transformation while the reset animation is
+    // running, cancel the reset animation.
+    if (_controllerReset!.status == AnimationStatus.forward) {
+      _animateResetStop();
+    }
+  }
+
+  void _onInteractionEnd(ScaleEndDetails details) {
+    _animateResetInitialize();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controllerReset = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controllerReset?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(name),
+        title: Text(widget.name),
       ),
-      body: Center(
+      body: InteractiveViewer(
+        transformationController: _transformationController,
+        onInteractionStart: _onInteractionStart,
+        onInteractionEnd: _onInteractionEnd,
         child: Hero(
-          tag: 'gambar-$name',
-          child: InteractiveViewer(child: Image.network(imageSrc)),
+          tag: 'gambar-${widget.name}',
+          child: Center(child: Image.network(widget.imageSrc)),
         ),
       ),
     );
